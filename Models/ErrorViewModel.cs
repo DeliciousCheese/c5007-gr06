2495364a06b86fa98287548c81990cd384363c01
Nathan Johnson <9967618@bcs.net.nz>
2017-09-13 13:04:46 +1200

    MVC Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace c5007_gr06.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
